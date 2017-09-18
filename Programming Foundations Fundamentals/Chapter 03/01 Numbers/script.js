00811e08662c1a9fc831c2a37f2e2d700acbe007
cam.w.w <cameronww7@users.noreply.github.com>
2017-09-18 15:14:50 -0700

    Starting Chapter 4

---
+++ Programming Foundations Fundamentals/Chapter 03/01 Numbers/script.js
@@ -0,0 +1,20 @@
+var a;
+a = 5;
+a = 10000000;
+a = 123.456;
+a = -500;
+alert(a);
+
+var b = 123;
+
+var c = "123";
+
+var z = true;
+var x = false;
+
+
+var name = prompt("what is your Face?");
+
+var message = "Hello";
+
+alert(message + name);
---
+++ Programming Foundations Fundamentals/Chapter 03/03 Comments/script.js
@@ -0,0 +1,21 @@
+// COmments AHHH
+var a = 5;
+var b = 10;
+var c = 20; 
+
+//Booleanssss WHATTT
+var x = true;
+var z = false;
+var name = prompt("What is your name?"); // comments
+var message = "Hello, ";
+alert(message + " " + name);
+
+/*
+CRAZY
+THiS
+IS 
+A 
+LArGE 
+COMMENT
+
+*/
