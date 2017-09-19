761bc4462ba8314415deae23cf3d5863676de078
cam.w.w <cameronww7@users.noreply.github.com>
2017-09-18 17:52:52 -0700

    finishing Chapter 6

--- Programming Foundations Fundamentals/Chapter 06/While loop/script.js
+++ Programming Foundations Fundamentals/Chapter 06/While loop/script.js
@@ -1,1 +1,29 @@
+var a = 1;
 
+while ( a < 2 ) {
+    alert( a );
+    a++;
+}
+
+for ( a; a < 3; a++ ) {
+    alert( a );
+}
+
+do {
+    alert( a );
+    a++;
+} while ( a < 4 );
+
+var amount = 0;
+var index = 0;
+
+// while ( index < 10 ) {
+//     amount = amount + 100;
+//     index++;
+// }
+
+for ( var index2 = 0; index2 < 10; index2++ ) {
+    amount = amount + 100;
+}
+
+alert( "the value is: " + amount );
