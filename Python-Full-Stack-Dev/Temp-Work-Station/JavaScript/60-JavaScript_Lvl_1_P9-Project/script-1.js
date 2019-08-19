7968306e5cf5d444e691a5735262c5d551257c6d
Cameron <cameronww7@users.noreply.github.com>
2019-08-19 13:31:23 -0700

    Level 1 JavaScript Project finished

---
+++ Python-Full-Stack-Dev/Temp-Work-Station/JavaScript/60-JavaScript_Lvl_1_P9-Project/script-1.js
@@ -0,0 +1,13 @@
+var firstName = prompt("What is your first name?");
+var lastName  = prompt("What is your last name?");
+var age       = prompt("What is your age");
+var height    = prompt("What is your height");
+var petName   = prompt("What is your Pet's Name");
+
+
+if (firstName[0] == lastName[0] &&
+    age >= 20 && age <= 30 &&
+    height >= 170 &&
+    petName == "Sammy") {
+      console.log("HACK THE PLANET!!!")
+    }
