67d152f06ac3835e7734766d0aac551d594a0114
Cameron <cameronww7@users.noreply.github.com>
2019-08-19 09:45:15 -0700

    Finished Exerise - My Solution

--- Python-Full-Stack-Dev/Temp-Work-Station/JavaScript/52-JavaScript_Lvl_1_P3/script-1.js
+++ Python-Full-Stack-Dev/Temp-Work-Station/JavaScript/52-JavaScript_Lvl_1_P3/script-1.js
@@ -1,4 +1,10 @@
-var name = prompt("What is your name?");
-alert("Hello " + name);
-
 // This is a comment
+
+alert("welcome!")
+var weightLbs = prompt("What is the weight in pounds (lbs)?")
+
+var weightKlo = weightLbs * .454
+
+alert("That is " + weightKlo + "kilograms")
+
+console.log("Conversion Completed")
