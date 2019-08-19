58dc0c13eb657ad69f253916af2f808e995cef9b
Cameron <cameronww7@users.noreply.github.com>
2019-08-19 09:40:25 -0700

    JavaScript - p1 done

---
+++ Python-Full-Stack-Dev/Temp-Work-Station/JavaScript/51-JavaScript_Lvl_1_P2/script-1.js
@@ -0,0 +1,8 @@
+// This is a comment
+
+alert("Welcome to your bank")
+var deposit = prompt("How much would you like to deposit")
+
+alert("You've deposited: " + deposit)
+
+console.log("You are a cool person!!!")
