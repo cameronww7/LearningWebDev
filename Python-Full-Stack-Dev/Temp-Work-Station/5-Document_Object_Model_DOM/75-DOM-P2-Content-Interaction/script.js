d673050bc07ec53ab863ff8f65014f694cb2ad86
Cameron <cameronww7@users.noreply.github.com>
2019-08-27 11:04:44 -0700

    Finished Content Interaction!

--- Python-Full-Stack-Dev/Temp-Work-Station/5-Document_Object_Model_DOM/75-DOM-P2-Content-Interaction/script.js
+++ Python-Full-Stack-Dev/Temp-Work-Station/5-Document_Object_Model_DOM/75-DOM-P2-Content-Interaction/script.js
@@ -6,7 +6,7 @@ var x = document.querySelector("p")
 x.textContent
 
 // Reassign Text
-x.textContent = "new"
+x.textContent = "new Text"
 
 // Refresh the page
 // Show actual HTML
@@ -20,10 +20,14 @@ x.innerHTML = "This is <strong>BOLD</strong>"
 /////////////////
 // Attributes //
 ///////////////
-
+// Edits a special id that has been set in the html
 var special = document.querySelector("#special")
+
+// This will look for an anchor tag and return its location
 var specialA = y.querySelector("a")
 
+// Finds the href only
 specialA.getAttribute("href")
 
+// This changes the href to amazon
 specialA.setAttribute("href","https://www.amazon.com")
