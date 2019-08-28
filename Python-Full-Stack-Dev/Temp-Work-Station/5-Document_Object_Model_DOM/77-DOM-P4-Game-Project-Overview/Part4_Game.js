6b175a6183400902643917ba61119e9282ac0b7c
Cameron <cameronww7@users.noreply.github.com>
2019-08-28 10:15:15 -0700

    Cleaned up / Editted

--- Python-Full-Stack-Dev/Temp-Work-Station/5-Document_Object_Model_DOM/77-DOM-P4-Game-Project-Overview/Part4_Game.js
+++ Python-Full-Stack-Dev/Temp-Work-Station/5-Document_Object_Model_DOM/77-DOM-P4-Game-Project-Overview/Part4_Game.js
@@ -4,32 +4,29 @@ var restart = document.querySelector('#b');
 // Grab all the squares
 var squares = document.querySelectorAll("td");
 
-
 // Clear Squares Function
 function clearBoard() {
-  for (var i = 0; i < squares.length; i++) {
-      squares[i].textContent = '';
+  for (var index = 0; index < squares.length; index++) {
+      squares[index].textContent = '';
   }
+};
 
-}
-restart.addEventListener('click',clearBoard)
-
-
-
+// is an Event Listener that will clean the board if you click restart!
+restart.addEventListener('click', clearBoard)
 
 // Create a function that will check the square marker
-function changeMarker(){
-    if(this.textContent === ''){
+function changeMarker() {
+    if (this.textContent === '') {
       this.textContent = 'X';
       // console.log(marker)
-    }else if (this.textContent === 'X') {
+    } else if (this.textContent === 'X') {
       this.textContent = 'O';
-    }else {
+    } else {
       this.textContent = '';
     }
 };
 
 // Use a for loop to add Event listeners to all the squares
-for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', changeMarker);
-}
+for (var index = 0; index < squares.length; index++) {
+    squares[index].addEventListener('click', changeMarker);
+};
