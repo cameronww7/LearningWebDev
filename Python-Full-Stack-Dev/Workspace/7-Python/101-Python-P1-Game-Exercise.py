5096f9812c3441458bf3a4529f78a6d73df0bca0
Cameron <cameronww7@users.noreply.github.com>
2019-09-04 13:33:17 -0700

    Finished

--- Python-Full-Stack-Dev/Workspace/7-Python/101-Python-P1-Game-Exercise.py
+++ Python-Full-Stack-Dev/Workspace/7-Python/101-Python-P1-Game-Exercise.py
@@ -21,15 +21,61 @@
 # There are a few things you will have to discover for yourself for this game!
 # Here are some useful hints:
 
-# Try to figure out what this code is doing and how it might be useful to you
 import random
-digits = list(range(10))
-random.shuffle(digits)
-print(digits[:3])
 
-# Another hint:
-guess = input("What is your guess? ")
-print(guess)
+def get_guess():
+    '''
+    Asks for the number guess and transforms the string to a list.
+    '''
+    return list(input("What is your guess?"))
 
-# Think about how you will compare the input to the random number, what format
-# should they be in? Maybe some sort of sequence? Watch the Lecture video for more hints!
+def generate_code():
+    '''
+    generates a 3 digit list for the code
+    '''
+    digits = [str(num) for num in range(10)]
+    random.shuffle(digits)
+    return digits[:3]
+
+def generate_clues(code,userGuess):
+    '''
+    Takes in a user guess and code then compares the numbers in a loop and
+    creates a list of clues according to the matching parameters.
+    '''
+    if userGuess == code:
+        return "CODE CRACKED"
+
+    clues = []
+
+    # Compare guess to code
+    for ind,num in enumerate(userGuess):
+        if num == code[ind]:
+            clues.append("Match")
+        elif num in code:
+            clues.append("Close")
+    if clues == []:
+        return ["Nope"]
+    else:
+        return clues
+
+# Run Game
+print("Welcome Code Breaker! Let's see if you can guess my 3 digit number!")
+
+# Create a Secret Code to start the Game
+secretCode = generate_code()
+print("Code has been generated, please guess a 3 digit number")
+print(secretCode)
+
+# Empty Clue Report to Start with
+clueReport = []
+
+# Keep asking until the user has gotten it right!
+while clueReport != "CODE CRACKED":
+    # Ask for guess
+    guess = get_guess()
+
+    # Give the clues
+    clueReport = generate_clues(guess,secretCode)
+    print("Here is the result of your guess:")
+    for clue in clueReport:
+        print(clue)
