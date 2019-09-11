60895f7999dc36bd732ee98d5f5f8230bfd8fa3e
Cameron <cameronww7@users.noreply.github.com>
2019-09-11 14:45:15 -0700

    Added Comments on where Url patterns for the pages

--- Python-Full-Stack-Dev/Workspace/8-Django/D_123_Django_Lvl1_Challenge_Template/D_123_Django_Lvl1_Challenge_Template/urls.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_123_Django_Lvl1_Challenge_Template/D_123_Django_Lvl1_Challenge_Template/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # Add Includes
 from appTwo import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('help/', include('appTwo.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name="index"), # Finds View Page
+    path('help/', include('appTwo.urls')), # Help Page
+    path('admin/', admin.site.urls), # Admin Page
 ]
