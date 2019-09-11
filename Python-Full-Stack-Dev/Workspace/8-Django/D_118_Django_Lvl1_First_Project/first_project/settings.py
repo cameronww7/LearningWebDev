648cf7555f3f82207d7201ec5799e9630eed3e26
Cameron <cameronww7@users.noreply.github.com>
2019-09-11 15:19:56 -0700

    Added a static image, using static files

--- Python-Full-Stack-Dev/Workspace/8-Django/D_118_Django_Lvl1_First_Project/first_project/settings.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_118_Django_Lvl1_First_Project/first_project/settings.py
@@ -25,6 +25,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
 print("\n > os.path.join(BASE_DIR,\"templates\")\n > ", os.path.join(BASE_DIR,"templates"))
 
+STATIC_DIR = os.path.join(BASE_DIR,"static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -129,3 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# used if you want to have multiple static file locations
+STATICFILES_DIR = [
+    STATIC_DIR,
+]
