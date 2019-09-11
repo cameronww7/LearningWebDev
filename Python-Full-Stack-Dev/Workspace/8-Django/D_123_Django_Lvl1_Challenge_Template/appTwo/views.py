5c1f9faca3ae7a0e4038370909d272d12d823b31
Cameron <cameronww7@users.noreply.github.com>
2019-09-11 14:48:04 -0700

    Explainds new View Code

--- Python-Full-Stack-Dev/Workspace/8-Django/D_123_Django_Lvl1_Challenge_Template/appTwo/views.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_123_Django_Lvl1_Challenge_Template/appTwo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Spits out My Second Project
 def index(request):
     return HttpResponse("<em>My Second Project</em>")
 
+# Adds Help Page
 def help(request):
     helpdict = {'help_insert':'HELP PAGE'}
     return render(request,'appTwo/help.html',context=helpdict)
