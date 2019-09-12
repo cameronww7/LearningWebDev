7adcbf9105a8cf72678c5f7a213297b3b9d7cf4c
Cameron <cameronww7@users.noreply.github.com>
2019-09-12 13:35:20 -0700

    Setup Models classes

--- Python-Full-Stack-Dev/Workspace/8-Django/D_127_Django_Lvl2_Models_1/first_project/first_app/models.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_127_Django_Lvl2_Models_1/first_project/first_app/models.py
@@ -16,13 +16,17 @@ from django.db import models
 # quit()
 
 # Create your models here.
+
+# Instance of Topic
 class Topic(models.Model):
     top_name = models.CharField(max_length=264,unique=True)
 
     def __str__(self):
         return self.top_name
 
+# Insance of Webpage
 class Webpage(models.Model):
+    # these are the columns in the webpage instance
     topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
     name = models.CharField(max_length=264,unique=True)
     url = models.URLField(unique=True)
@@ -30,7 +34,9 @@ class Webpage(models.Model):
     def __str__(self):
         return self.name
 
+# Instance of AccessRecord
 class AccessRecord(models.Model):
+    # More columns
     name = models.ForeignKey(Webpage,on_delete=models.CASCADE)
     date = models.DateField()
 
