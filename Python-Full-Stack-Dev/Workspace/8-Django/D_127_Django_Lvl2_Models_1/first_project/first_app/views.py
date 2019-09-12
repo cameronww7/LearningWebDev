b86d44fd3165ac7cec45f045c8eda7ae0301fd1f
Cameron <cameronww7@users.noreply.github.com>
2019-09-12 12:31:47 -0700

    Editted views to show its workings

--- Python-Full-Stack-Dev/Workspace/8-Django/D_127_Django_Lvl2_Models_1/first_project/first_app/views.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_127_Django_Lvl2_Models_1/first_project/first_app/views.py
@@ -9,12 +9,12 @@ from .models import Topic,Webpage,AccessRecord
 # Our original index view function
 # corresponds to indexOLD.html (rename it to index.html to use it!)
 
-# def index(request):
-#     my_dict = {'insert_me':"Hello I am from views.py!"}
-#     return render(request,'first_app/index.html',context=my_dict)
-
-
 def index(request):
-    webpages_list = AccessRecord.objects.order_by('date')
-    date_dict = {"access_records":webpages_list}
-    return render(request,'first_app/index.html',date_dict)
+    my_dict = {'insert_me':"Hello I am from views.py!"}
+    return render(request,'first_app/index.html',context=my_dict)
+
+
+# def index(request):
+#     webpages_list = AccessRecord.objects.order_by('date')
+#     date_dict = {"access_records":webpages_list}
+#     return render(request,'first_app/index.html',date_dict)
