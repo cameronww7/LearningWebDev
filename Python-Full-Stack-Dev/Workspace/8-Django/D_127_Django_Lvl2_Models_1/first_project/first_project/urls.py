9427798330a646601fa89bf96eb6fd3069f3010d
Cameron <cameronww7@users.noreply.github.com>
2019-09-12 13:04:22 -0700

    Pointed out Comment block explaing what we have done so far

--- Python-Full-Stack-Dev/Workspace/8-Django/D_127_Django_Lvl2_Models_1/first_project/first_project/urls.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_127_Django_Lvl2_Models_1/first_project/first_project/urls.py
@@ -2,14 +2,18 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
+
 Examples:
-Function views
+
+Function views ### Very First App
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+
+Class-based views ### Second one
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+
+Including another URLconf ### Third example now.
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
