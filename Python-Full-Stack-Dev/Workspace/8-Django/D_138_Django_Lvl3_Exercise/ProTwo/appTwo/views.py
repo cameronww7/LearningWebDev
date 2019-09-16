fb51c42a68f58e12983eff5c6b8bf3e017082a9e
Cameron <cameronww7@users.noreply.github.com>
2019-09-16 15:25:05 -0700

    Finished lvl 4 Django

--- Python-Full-Stack-Dev/Workspace/8-Django/D_138_Django_Lvl3_Exercise/ProTwo/appTwo/views.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_138_Django_Lvl3_Exercise/ProTwo/appTwo/views.py
@@ -8,12 +8,14 @@ def index(request):
     return render(request,'appTwo/index.html')
 
 def users(request):
-
+    # Create new User form
     form = NewUserForm()
 
+    # if someone hits sumbit we check
     if request.method == "POST":
         form = NewUserForm(request.POST)
 
+        # If is valid, we save the form
         if form.is_valid():
             form.save(commit=True)
             return index(request)
