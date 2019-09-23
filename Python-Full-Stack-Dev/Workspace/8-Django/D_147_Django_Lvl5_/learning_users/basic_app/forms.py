056f4b788420d9ddd0e48d90e90440ec5fa75e14
Cameron <cameronww7@users.noreply.github.com>
2019-09-23 15:57:49 -0700

    Finished setting up html pages

--- Python-Full-Stack-Dev/Workspace/8-Django/D_147_Django_Lvl5_/learning_users/basic_app/forms.py
+++ Python-Full-Stack-Dev/Workspace/8-Django/D_147_Django_Lvl5_/learning_users/basic_app/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfileInfo
 
+# Takes in users password
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
+    # Basic meta information about user
     class Meta():
         model = User
         fields = ('username','email','password')
 
-
+# basic user pofile relationship
 class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
